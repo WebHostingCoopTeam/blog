b7fe91eaee8d1670083f01d011319efada26487c
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-05-03 12:44:11 -0500

    allinone

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env -vS bash -l
+sudo apt update
+sudo apt-get upgrade -yq
+sudo apt-get install -yq curl git build-essential
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+curl -sL https://raw.githubusercontent.com/joshuacox/cloner/refs/heads/main/bootstrapcloner.sh | bash
 export NVM_DIR="${HOME}/.nvm"
-export PNPM_HOME=/home/thoth/.local/share/pnpm
-#export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [[ -r $NVM_DIR/bash_completion ]] && \. $NVM_DIR/bash_completion
 : ${BLOG_PATH:=$HOME}
+
 set -eux
 nvm install lts/jod
 npm i -g yarn
