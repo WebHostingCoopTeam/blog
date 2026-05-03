61c3186103b8b52763b4218b6862f5e9ea0f3626
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2026-05-03 12:19:37 -0500

    nvm

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env -vS bash -l
+export NVM_DIR="/home/thoth/.nvm"
+export PNPM_HOME=/home/thoth/.local/share/pnpm
+#export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[[ -r $NVM_DIR/bash_completion ]] && \. $NVM_DIR/bash_completion
 : ${BLOG_PATH:=$HOME}
 set -eux
 nvm install lts/jod
